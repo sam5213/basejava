72f8af04925691572725424eb6d41d66c225bda7
sam5213 <>
2022-12-21 00:44:26 +0300

    realize methods in ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,73 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int index = 0;
 
     void clear() {
+        for (int i = 0; i < index; i++) {
+            storage[i] = null;
+        }
+        index = 0;
     }
 
     void save(Resume r) {
-    }
+        if (index == 10000) {
+            System.out.println("Storage filled");
+            return;
+        }
+        if (index > 1) {
+            for (int i = 0; i < index; i++) {
+                if (storage[i].uuid == r.uuid) {
+                    System.out.println("This uuid is already in the storage");
+                    return;
+                }
+            }
+        }
+        storage[index++] = r;
+     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+            }
+        }
+        if (resume == null) {
+            System.out.println("Resume not found ");
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int deleteIndex = 0;
+        for (int i = 0; i < index; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                deleteIndex = i;
+                break;
+            }
+        }
+        for (int i = deleteIndex; i < index + 1; i++) {
+            storage[i] = storage[i + 1];
+        }
+        index--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] partResume = new Resume[index];
+        int j = 0;
+        for (int i = 0; i < index; i++) {
+            if (storage[i] != null) {
+                partResume[j++] = storage[i];
+            }
+        }
+        return partResume;
     }
 
     int size() {
-        return 0;
+        return index;
     }
 }
